5e02295b51379a63a68b9a83940e8359d82838ab
Oran C <Oranbusiness@gmail.com>
2020-10-01 13:35:03 -0700

    [mvp] working encoding generator

--- sketch.js
+++ sketch.js
@@ -137,14 +137,15 @@ function setup() {
     // encoding: 1111111111116 [6 being the check digit]
     // See readme for a fuller breakdown of the numbers here
     
-    example_bit_pattern =
-    "101" + //left  sentinel
-    "0011001"+ "0011001"+ "0110011"+ "0011001"+ "0110011"+ "0110011"+ //left encoded
-    "01010"+ //center
-    "1100110"+ "1100110"+ "1100110"+ "1100110"+ "1100110"+ //right encoded
-    "1010000"+ //check digit (6)
-    "101"//right sentinel
-    
+    // example_bit_pattern =
+    // "101" + //left  sentinel
+    // "0011001"+ "0011001"+ "0110011"+ "0011001"+ "0110011"+ "0110011"+ //left encoded
+    // "01010"+ //center
+    // "1100110"+ "1100110"+ "1100110"+ "1100110"+ "1100110"+ //right encoded
+    // "1010000"+ //check digit (6)
+    // "101"//right sentinel
+    example_bit_pattern = generate_bit_pattern("7501031311309")
+    print(example_bit_pattern)
     // print(example_bit_pattern)
     // bits_to_bars(string_to_array(example_bit_pattern), width / 3, width / 3)
     // example_bit_pattern = string_to_array(example2());
@@ -172,6 +173,22 @@ function draw() {
 //  or
 // print out this page then scan`,6);
 }
+
+let generate_bit_pattern = (number_string) => {
+    pattern_number = parseInt(number_string[0])
+    encding_pattern = num_structure[pattern_number]
+    number_string = number_string.slice(1)
+    left_group = number_string.slice(0,6)
+    right_group = number_string.slice(6,12)
+    // print(left_group,", ",right_group)
+    let left_bit_pattern = map_structure_to_bits(encding_pattern, left_group);
+    let right_bit_pattern = map_structure_to_bits("RRRRRR",right_group);
+    // print(left_bit_pattern,",",right_bit_pattern)
+    let full_bit_pattern =
+        "101" + left_bit_pattern + "01010" + right_bit_pattern + "101";
+        return full_bit_pattern
+
+};
 
 let generate_barcode = (location)=>{
     // bit_pattern = string_to_array(example_bit_pattern);
